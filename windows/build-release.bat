ce16393311cbaf0c80ff85b299721dcf159b759b
Mark Andrae <ma@protel.net>
2018-09-16 19:50:53 +0200

    Easy mode for windows dev

---
+++ windows/build-release.bat
@@ -0,0 +1,5 @@
+cd /d %~d0%~p0
+
+bin\arm-none-eabi-as.exe -o ..\out\crt0.o ..\crt0.s
+bin\arm-none-eabi-ld.exe -T ..\linker.ld -o ..\out\snake.elf ..\out\crt0.o ..\target\gba\release\libgba_snake.a
+bin\arm-none-eabi-objcopy.exe -O binary ..\out\snake.elf ..\out\snake.gba

30ea71c0527414bb33ee5c9d552e9301a34a7e1a
Mark Andrae <ma@neopoly.de>
2018-09-17 11:18:15 +0200

    Build with one step on windows (rust nightly-gnu rust.src and xargo needs to be installed)

--- windows/build-release.bat
+++ windows/build-release.bat
@@ -1,5 +1,9 @@
+@echo off
 cd /d %~d0%~p0
-
+cd ..
+xargo build --release --target=gba
+cd windows
 bin\arm-none-eabi-as.exe -o ..\out\crt0.o ..\crt0.s
 bin\arm-none-eabi-ld.exe -T ..\linker.ld -o ..\out\snake.elf ..\out\crt0.o ..\target\gba\release\libgba_snake.a
 bin\arm-none-eabi-objcopy.exe -O binary ..\out\snake.elf ..\out\snake.gba
+cd ..
